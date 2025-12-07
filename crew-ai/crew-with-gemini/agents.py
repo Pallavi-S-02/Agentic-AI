d805b5bfd05ec3e75f9c9eb2ae197df220d2b9d3
Pallavi Anil Sindkar <83487183+Pallavi-S-02@users.noreply.github.com>
2025-12-07 23:53:31 +0530

    Add files via upload

---
+++ crew-ai/crew-with-gemini/agents.py
@@ -0,0 +1,48 @@
+from crewai import Agent, LLM
+from tools import tool 
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+#call gemini model
+llm = LLM(
+    model="gemini/gemini-1.5-flash", 
+    verbose=True,
+    temperature=0.5,
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+#Create a senior researcher agent with memory and verbose mode
+news_researcher=Agent(
+    role="Senior Researcher",
+    goal="Uncover ground breaking technologies in {topic}",
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Driven by curiosity, you're at the forefront of"
+        "innovation, eager to explore and share knowledge that could change"
+        "the world."
+    ),
+    tools=[tool],
+    llm=llm,
+    allow_delegation=True
+)
+
+#Create a write agent with custom tools responsible in writing news blogs
+
+news_writer = Agent(
+    role='Writer',
+    goal="NArrate compelling tech stories about {topic}",
+    verbose=True,
+    memory=True,
+    backstory=(
+    "With a flair for simplifying complex topics, you craft"
+    "engaging narratives that captivate and educate, bringing new"
+    "discoveries to light in an accessible manner."
+    ),
+    tools=[tool],
+    llm=llm,
+    allow_delegation=False
+)
